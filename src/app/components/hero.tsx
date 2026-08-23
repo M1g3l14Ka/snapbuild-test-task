4245868c232fd6f95f06697c49b3df84868bd284
M1g3l14Ka <katcion00@mail.ru>
2026-08-23 23:52:27 +0300

    changed styles

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,15 +6,26 @@ export default function Hero() {
     
     return (
         <div
-            className="w-full z-10 h-20 text-black font-bold font-sans rounded-2xl sticky top-0 bg-zinc-100 flex justify-between items-center m-2 p-4"
+            className="w-full z-10 h-21 rounded-2xl sticky top-0 flex justify-between items-center p-4 bg-white"
         >
-            <div>
+            <div className="flex justify-center items-center p-2">
                 <Image
-                    width={50}
-                    height={50}
-                    src='/logo.webp'
+                    width={35}
+                    height={35}
+                    src='/snap-logo.webp'
                     alt=""
                 />
+                <span className="text-2xl font-bold">снэпбилд</span>
+            </div>
+
+            <div
+                className=""
+            >
+                <button
+                    className="bg-[#050505] text-white p-2.5 px-5 rounded-xl cursor-pointer"
+                >
+                    Начать сейчас
+                </button>
             </div>
 
         </div>
